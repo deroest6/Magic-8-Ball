95c3799e63fbd741bd3165369ae2f15f65a80b58
Jantsen <deroest6@hotmail.com>
2022-03-17 19:36:42 -0600

    Initial Commit - Also added background, button and interactive image

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var askButton: UIImageView!
+//    askButton.layer.cornerRadius = 10.0
+//    askButton.layer.clipsToBounds = true
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        imageView.image = ballArray.randomElement()
+        
+    }
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
+    var ballArray = [#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball2")]
+    
+    
 
 }
 
